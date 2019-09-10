626e90acb8c82194f521f305190da3ee7d95ffaa
Brett Delle Grazie <brett.dellegrazie@gmail.com>
2019-09-10 22:04:52 +0100

    Teach asdf role to handle plugin URLs too

---
+++ filter_plugins/mapattributes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+class FilterModule(object):
+    def filters(self):
+        return { 'mapattributes': self.mapattributes }
+
+    def mapattributes(self, list_of_dicts, list_of_keys):
+        l = []
+        for di in list_of_dicts:
+            newdi = { }
+            for key in list_of_keys:
+                if key in di:
+                    newdi[key] = di[key]
+            l.append(newdi)
+        return l
